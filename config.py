0987a89d586de6f24f9fb18a9e514906f5631366
Alessandro Ciocchetti <63201940+PaleAlex@users.noreply.github.com>
2022-10-31 22:50:07 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from google.cloud import storage
 
 api_key = os.getenv("GOOGLE_MAPS")
 PATH = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-#os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = PATH
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = PATH
 storage_client = storage.Client(PATH)
 bucket = storage_client.get_bucket('choosing-storage')
 #root = "root_user"
