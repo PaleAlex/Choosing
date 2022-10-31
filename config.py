556d76517b2b94f2681b49d46433f58c4c5ba64e
Alessandro Ciocchetti <63201940+PaleAlex@users.noreply.github.com>
2022-10-31 23:53:36 +0100

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from google.cloud import storage
-api_key = os.environ['API_KEY_MAPS']
+api_key = "AIzaSyABp7bQQJs0mixV7XANFmpZlOUxSa0n1Ys"
 PATH = os.path.join(os.getcwd(), 'skilled-bonus-284420-d6d56288fa20.json')
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = PATH
 storage_client = storage.Client(PATH)
